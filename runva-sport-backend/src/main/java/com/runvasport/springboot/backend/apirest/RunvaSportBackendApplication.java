c10f29b5431538d10bafa8636ddb6b1f93866e23
sergiopuce93 <sergiomu.garcia@gmail.com>
2019-07-09 15:38:34 +0200

    Create spring project

---
+++ runva-sport-backend/src/main/java/com/runvasport/springboot/backend/apirest/RunvaSportBackendApplication.java
@@ -0,0 +1,13 @@
+package com.runvasport.springboot.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunvaSportBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunvaSportBackendApplication.class, args);
+	}
+
+}
