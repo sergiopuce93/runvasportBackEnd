e3bcf62d9b6f3547dd618a6db3889d01b268bf78
sergiopuce93 <sergiomu.garcia@gmail.com>
2019-07-09 17:27:16 +0200

    add controllers,model,service,model to carrera

---
+++ runva-sport-backend/src/main/java/com/runvasport/springboot/backend/apirest/models/Carrera.java
@@ -0,0 +1,5 @@
+package com.runvasport.springboot.backend.apirest.models;
+
+public class Carrera {
+
+}
---
+++ runva-sport-backend/src/main/java/com/runvasport/springboot/backend/apirest/services/ICarreraService.java
@@ -0,0 +1,5 @@
+package com.runvasport.springboot.backend.apirest.services;
+
+public class ICarreraService {
+
+}
