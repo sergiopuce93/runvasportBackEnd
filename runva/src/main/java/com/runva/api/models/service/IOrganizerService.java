2d9d487202a92bf75213e451f0fff752f0e49082
Daniel Villoldo <villoldinerreape@gmail.com>
2019-07-31 18:46:37 +0200

    Added service "Organizing services"

--- runva/src/main/java/com/runva/api/models/service/IOrganizerService.java
+++ runva/src/main/java/com/runva/api/models/service/IOrganizerService.java
@@ -1,5 +1,54 @@
 package com.runva.api.models.service;
 
-public class IOrganizerService {
+import java.util.List;
 
+import com.runva.api.models.entity.Organizer;
+
+
+/**
+ * Service class for organizerDao class
+ * 
+ *
+ */
+
+public interface IOrganizerService {
+
+	/**
+	 * Get organizer's list
+	 * 
+	 * @return organizer's list
+	 */
+	public List<Organizer> getAll();
+
+	/**
+	 * Get organizer by id
+	 * 
+	 * @param id
+	 * @return organizer
+	 */
+	public Organizer getOrganizerById(Integer id);
+
+	/**
+	 * Create new organizer
+	 * 
+	 * @param organizer
+	 * @return organizer
+	 */
+	public Organizer newOrganizer(Organizer organizer);
+
+	/**
+	 * Delete organizer by id
+	 * 
+	 * @param id
+	 * @return
+	 */
+	public Organizer delete(Integer id);
+	
+	/**
+	 * Update organizer
+	 * 
+	 * @param organizer
+	 * @return
+	 */
+	public Organizer update(Organizer organizer);
 }
