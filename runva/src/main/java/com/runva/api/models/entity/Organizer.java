f1f87c714def108640b6fc7f350df04ba6eaec28
sergio <sergiomu.garcia@gmail.com>
2019-08-01 00:06:45 +0200

    fixed warning

--- runva/src/main/java/com/runva/api/models/entity/Organizer.java
+++ runva/src/main/java/com/runva/api/models/entity/Organizer.java
@@ -1,19 +1,15 @@
 package com.runva.api.models.entity;
 
-import java.util.Date;
+
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
-import javax.persistence.ForeignKey;
+
 
 
 /**
